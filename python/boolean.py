3dd5c6e1df370202b7adec1c8ac63502411fdffd
Naito273 <naitore2kimathi@gmail.com>
2023-05-08 07:12:09 +0300

    boolean

---
+++ python/boolean.py
@@ -0,0 +1,12 @@
+#booleans represent one of two values true or false
+#print(10 > 9)
+#print(10==9)
+#print(10 < 9)
+#output true false false
+a = 200
+b = 33
+
+if b > a:
+    print("b is greater than a")
+else:
+    print("b is not greater than a") 
